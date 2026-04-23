0d5db579898747385ef26ab6894e999782723e88
Adamiscool123 <adamidrissi177@gmail.com>
2026-04-23 14:50:19 +0100

    Added base

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Dashboard from "./Dashboard.jsx";
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/Dashboard.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "Sidebar.jsx";
+
+function Dashboard() {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+}
+
+export default Dashboard;
