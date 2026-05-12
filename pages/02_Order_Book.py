6195c83154a9d87c3b5e11d794a9a7d8da3690f9
Adamiscool123 <adamidrissi177@gmail.com>
2026-05-12 21:51:44 +0100

    made printing work

--- pages/02_Order_Book.py
+++ pages/02_Order_Book.py
@@ -1,6 +1,14 @@
 import Variable
 import streamlit as st
+import time
 
 v = st.session_state.variables
 
 v.print_book()
+
+if v.simulation:
+    v.run_simulation(st.session_state.market)
+
+if v.simulation:
+    time.sleep(0.5)
+    st.rerun()
